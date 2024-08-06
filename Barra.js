15910f1908e2359360cd2ecd644abafc03fbb48a
johnathanhenriq <j.henriquecl11@gmail.com>
2024-08-05 23:59:10 -0300

    commit das classes (teste)

---
+++ Barra.js
@@ -0,0 +1,22 @@
+class Barra {
+    constructor(x, y, width, height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.speed = 10;
+    }
+
+    draw(context) {
+        context.fillStyle = 'white';
+        context.fillRect(this.x, this.y, this.width, this.height);
+    }
+
+    moveUp() {
+        this.y = Math.max(this.y - this.speed, 0);
+    }
+
+    moveDown(canvas) {
+        this.y = Math.min(this.y + this.speed, canvas.height - this.height);
+    }
+}
